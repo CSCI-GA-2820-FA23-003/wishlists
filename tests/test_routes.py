914d210e3ba5cf5dfe14e96afe69613e68fa36d6
studentgrad0419 <mc7303@nyu.edu>
2023-10-11 08:01:32 -0400

    refactor to wishlist in test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,5 @@
 """
-TestYourResourceModel API Service Test Suite
+Wishlist API Service Test Suite
 
 Test cases can be run with the following:
   nosetests -v --with-spec --spec-color
@@ -8,38 +8,46 @@ Test cases can be run with the following:
 import os
 import logging
 from unittest import TestCase
+from tests.factories import WishlistFactory
 from service import app
-from service.models import db
+from service.models import db, Wishlist
 from service.common import status  # HTTP Status Codes
+from service.routes import app
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
+
+BASE_URL = "/accounts"
 
 
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceServer(TestCase):
-    """ REST API Server Tests """
+class TestWishlistServer(TestCase):
+    """REST API Server Tests"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
+        """This runs once before the entire test suite"""
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
+        """This runs once after the entire test suite"""
 
     def setUp(self):
-        """ This runs before each test """
+        """This runs before each test"""
         self.client = app.test_client()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
 
     ######################################################################
-    #  P L A C E   T E S T   C A S E S   H E R E
+    #  W I S H L I S T   T E S T   C A S E S   H E R E
     ######################################################################
 
     def test_index(self):
-        """ It should call the home page """
+        """It should call the home page"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
