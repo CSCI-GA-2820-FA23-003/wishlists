53e1f20ecf3c0ef3e50038ba02aa20880f6c1ccf
studentgrad0419 <mc7303@nyu.edu>
2023-12-03 20:27:43 -0500

    removed excess from test

--- features/steps/wishlist_item_ui_steps.py
+++ features/steps/wishlist_item_ui_steps.py
@@ -96,14 +96,8 @@ def step_impl(context, expected_rows, table_id):
                 (By.CSS_SELECTOR, table_selector)
             )
         )
-        print("table is vis: ", table_visible)
-        # Check if tbody element exists
-        tbody_tr_present = context.driver.find_elements(
-            By.CSS_SELECTOR, f"{table_selector} tbody tr"
-        )
-
+        
         rows_found = len(context.driver.find_elements(By.CSS_SELECTOR, f"{table_selector} tbody tr"))
-
         assert int(rows_found) == expected_rows
 
     except TimeoutException:
