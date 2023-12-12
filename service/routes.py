40e636b5ab60a94128298a31ecb93e438b27f870
Rahil Kadakia <rahilkadakia99@gmail.com>
2023-12-12 13:10:19 +0530

    code fix

--- service/routes.py
+++ service/routes.py
@@ -222,7 +222,7 @@ class WishlistsCollection(Resource):
         """Returns all of the Wishlists"""
         app.logger.info("Request for Wishlist lists")
 
-        customer_id = request.args.get("customer_id")
+        customer_id = request.args.get("customer-id")
 
         wishlists = []
 
@@ -396,57 +396,6 @@ class WishlistItemsCollection(Resource):
             status.HTTP_201_CREATED,
             {"Location": f"/wishlists/{wishlist.id}/items/{wishlist_item.id}"},
         )
-
-
-######################################################################
-# LIST ITEMS
-######################################################################
-# @app.route("/wishlists/<int:wishlist_id>/items", methods=["GET"])
-# def list_wishlist_items(wishlist_id):
-#     """Returns wishlist items based on query parameters"""
-#     app.logger.info(
-#         "Request for all WishlistItems for Wishlist with id: %s", wishlist_id
-#     )
-
-#     # See if the account exists and abort if it doesn't
-#     wishlist = Wishlist.find(wishlist_id)
-#     if not wishlist:
-#         abort(
-#             status.HTTP_404_NOT_FOUND,
-#             f"Wishlist with id '{wishlist_id}' could not be found.",
-#         )
-
-#     query_params = request.args.to_dict()
-
-#     # Initialize base query for items related to this wishlist
-#     base_query = WishlistItem.query.filter_by(wishlist_id=wishlist_id)
-
-#     # Check for query parameters and filter the base query accordingly
-#     if "product_id" in query_params:
-#         base_query = base_query.filter_by(product_id=query_params["product_id"])
-
-#     if "product_name" in query_params:
-#         base_query = base_query.filter(
-#             WishlistItem.product_name.ilike(f"%{query_params['product_name']}%")
-#         )
-
-#     if "product_price" in query_params:
-#         base_query = base_query.filter(
-#             WishlistItem.product_price <= query_params["product_price"]
-#         )
-
-#     if "quantity" in query_params:
-#         base_query = base_query.filter(
-#             WishlistItem.quantity <= query_params["quantity"]
-#         )
-
-#     if "created_date" in query_params:
-#         base_query = base_query.filter_by(created_date=query_params["created_date"])
-
-#     # Fetch the filtered results
-#     results = [item.serialize() for item in base_query.all()]
-
-#     return make_response(jsonify(results), status.HTTP_200_OK)
 
 
 ######################################################################
